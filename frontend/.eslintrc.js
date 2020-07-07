812a9c1cc654a5b08f018546f1ffab7997f94438
Niksa <juntusen.niko@gmail.com>
2020-07-07 19:57:37 +0300

    Eslint to frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2020': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 11,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        'react-hooks'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'warn' // Checks effect dependencies
+    },
+    'settings': {
+        'react': {
+            'version': 'detect'
+        }
+    }
+}
