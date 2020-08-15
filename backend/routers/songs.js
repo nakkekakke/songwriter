289002b932149ed382826052c9a4c9944c259cd2
Niksa <juntusen.niko@gmail.com>
2020-08-16 00:07:32 +0300

    Refactored songRouter to use async await

--- backend/routers/songs.js
+++ backend/routers/songs.js
@@ -2,54 +2,49 @@ const songRouter = require('express').Router()
 const Song = require('../models/song')
 //const songService = require('../services/songService')
 
-songRouter.get('/:id', (req, res, next) => {
-  Song.findById(req.params.id)
-    .then(song => {
-      song ? res.json(song.toJSON()) : res.status(404).end()
-    })
-    .catch(error => {
-      next(error)
-    })
+songRouter.get('/:id', async (req, res, next) => {
+  try {
+    const song = await Song.findById(req.params.id)
+    song ? res.json(song.toJSON()) : res.status(404).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
-songRouter.get('/', (req, res, next) => {
-  Song.find({})
-    .then(songs => {
-      res.json(songs.map(s => s.toJSON()))
-    })
-    .catch(error => {
-      next(error)
-    })
+songRouter.get('/', async (req, res, next) => {
+  try {
+    const songs = await Song.find({})
+    res.json(songs.map(s => s.toJSON()))
+  } catch (error) {
+    next(error)
+  }
 })
 
-songRouter.post('/', (req, res, next) => {
-  Song.create(req.body)
-    .then(song => {
-      res.json(song)
-    })
-    .catch(error => {
-      next(error)
-    })
+songRouter.post('/', async (req, res, next) => {
+  try {
+    const song = await Song.create(req.body)
+    res.json(song)
+  } catch (error) {
+    next(error)
+  }
 })
 
-songRouter.put('/:id', (req, res, next) => {
-  Song.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then(song => {
-      res.json(song)
-    })
-    .catch(error => {
-      next(error)
-    })
+songRouter.put('/:id', async (req, res, next) => {
+  try {
+    const song = await Song.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.json(song)
+  } catch (error) {
+    next(error)
+  }
 })
 
-songRouter.delete('/:id', (req, res, next) => {
-  Song.findByIdAndDelete(req.params.id)
-    .then(song => {
-      res.json(song)
-    })
-    .catch(error => {
-      next(error)
-    })
+songRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const song = await Song.findByIdAndDelete(req.params.id)
+    res.json(song)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = songRouter
